9f2d0e835514f0aecc6cbb31a7dabe6367d410bf
waldner <waldner@users.noreply.github.com>
2016-03-17 16:46:12 +0100

    Added files via upload

---
+++ ldap_auth.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Sys::Syslog qw(:standard :macros);
+use Net::LDAP;
+
+my $facility = LOG_AUTH;
+my $ourname = 'ldap_auth.pl';
+
+my $ldapserver = 'windowsdc';
+my $domain = 'example.com';
+my $vpngroup = 'vpnauth';
+
+# base DN for the search; adjust the code if the vpn group isn't directly in here.
+my $basedn = 'ou=Users,dc=example,dc=com';
+
+my $ldap_uri = "ldap://${ldapserver}.${domain}";
+
+# these are passed by OpenVPN
+my $username = $ENV{'username'};
+my $password = $ENV{'password'};
+
+openlog($ourname, 'nofatal,pid', $facility);
+
+my @filter = ( "(sAMAccountName=${username})",
+               "(memberOf=cn=${vpngroup},${basedn})",
+               '(accountStatus=active)',
+             );
+
+# bind as the authenticating user
+my $bindname = $username . '@' . $domain;
+
+syslog(LOG_INFO, "Attempting to authenticate user $username ($bindname)");
+
+my $ldap;
+
+if (not ($ldap = Net::LDAP->new($ldap_uri))) {
+  syslog(LOG_ERR, "Connect to $ldap_uri failed, error: %m");
+  closelog();
+  exit 1;
+}
+
+my $result = $ldap->bind($bindname, password => $password);
+
+if ($result->code()) {
+  syslog(LOG_ERR, "LDAP binding failed (wrong user/password?), error: " . $result->error);
+  closelog();
+  exit 1;
+}
+
+$result = $ldap->search( base => $basedn, filter => "(&" . join("", @filter) . ")" );
+
+if ($result->code()) {
+  syslog(LOG_ERR, "LDAP search failed, error: " . $result->error);
+  closelog();
+  exit 1;
+}
+
+my $count = $result->count();
+
+if ($count == 1) {
+  syslog(LOG_INFO, "User $username authenticated successfully");
+} else {
+  syslog(LOG_ERR, "User $username not authenticated (user not in group?)");
+}
+
+closelog();
+
+exit ($count == 1 ? 0 : 1);
---
+++ ldap_auth.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# passed by openvpn (in the environment):
+#
+# $username
+# $password
+
+ourname=ldap_auth.sh
+facility=auth
+
+ldapserver=windowsdc
+domain=example.com
+
+vpngroup=vpnauth
+basedn='ou=Users,dc=example,dc=com'
+
+bindname=${username}@${domain}
+
+declare -a query
+
+# writing the query this way is useful because it's easier to
+# include it in the log; using an array instead of a string is
+# safer, see http://mywiki.wooledge.org/BashFAQ/050
+
+query[0]='-L'
+query[1]='-s'
+query[2]='sub'
+query[3]='-x'
+query[4]='-w'
+query[5]="${password}"
+query[6]='-D'
+query[7]="${bindname}"
+query[8]='-b'
+query[9]="${basedn}"
+query[10]='-H'
+query[11]="ldap://${ldapserver}.${domain}"
+query[12]="(&(sAMAccountName=${username})(memberOf=cn=${vpngroup},${basedn})(accountStatus=active))"
+query[13]='dn'
+
+output=$(mktemp)
+error=$(mktemp)
+
+# clean temp files when we terminate
+trap "rm -f ${output} ${error}" EXIT
+
+logger -p "${facility}.info" -t "$ourname" "Trying to authenticate user ${bindname} against AD"
+
+ldapsearch "${query[@]}" 1>"${output}" 2>"${error}"
+
+# save exist status here, otherwise the following assignment resets $?
+status=$?
+
+query[5]='xxxxxxxxx'   # obfuscate password to put query in the logs
+
+if [ $status -ne 0 ]; then
+  logger -p "${facility}.err" -t "${ourname}" "There was an error authenticating user ${username} (${bindname}) against AD."
+  logger -p "${facility}.err" -t "${ourname}" "The query was: ldapsearch ${query[*]}"
+  logger -p "${facility}.err" -t "${ourname}" "The error was: $(tr '\n' ' ' < "${error}" )"  # turn multiline into single line
+  exit 1
+fi
+
+# look for the "numEntries" line in the output of ldapsearch
+numentries=$(awk '/numEntries:/{ne = $3} END{print ne + 0}' "$output")
+
+if [ $numentries -eq 1 ]; then
+  logger -p "${facility}.info" -t "{$ourname}" "User ${username} authenticated successfully"
+  exit 0
+else
+  logger -p "${facility}.err" -t "${ourname}" "User ${username} NOT authenticated (user not in group?)"
+  logger -p "${facility}.err" -t "${ourname}" "The query was: ldapsearch ${query[*]}"
+  exit 1
+fi
+
